6eaa60b1ea3814bf045e0d19c9b8ef34e4bf861f
sergiofrm <serginho.felipe@hotmail.com>
2022-10-14 21:04:40 -0300

    add style #2

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const tempo = setInterval(function time() {
+  let dateToday = new Date()
+  let hr = dateToday.getHours()
+  let mm = dateToday.getMinutes()
+  let ss = dateToday.getSeconds()
+})
